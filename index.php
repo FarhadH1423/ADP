1ad59581c1d4fb549e64c8a7d3f8540aac776c80
Farhad Hossain <16303038@iubat.edu>
2020-09-24 02:30:10 +0600

    project Initilization with Topbar

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>  
+    <title>ADP</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Top Bar start -->
+    <div id="top"> 
+        <div class="container">
+            <div class="col-md-6 offer">
+                <a href="#" class="btn btn-success btn-sm">
+                    Welcome Guest
+                </a>
+
+                <a href="#">Shopping Cart Total Price: INR 100, Total Items 2</a>
+            </div>
+            <div class="col-md-6">
+                <ul class="menu">
+                    <li>
+                    <a href="customer_registration.php"> Register </a>
+                    </li>
+
+                    <li>
+                    <a href="checkout.php"> My Account </a>
+                    </li>
+
+                    <li>
+                    <a href="cart.php"> Goto Cart </a>
+                    </li>
+
+                    <li>
+                    <a href="login.php"> Login </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Top Bar end -->
+    <div class="navbar navbar-default">
+        
+    </div>
+    
+</body>
+</html>
