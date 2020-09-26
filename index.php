95b9aeaf3eb213396f31bf18821fb7db5a08c3ea
Farhad Hossain <16303038@iubat.edu>
2020-09-27 02:17:05 +0600

    shop page with header and footer

--- index.php
+++ index.php
@@ -445,7 +445,7 @@
     ?>
 
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </body>
 </html>
